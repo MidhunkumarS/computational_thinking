4d55b08b80d4326be03c116fcabbb5cde9896e4a
Midhunkumar.S <34641347+MidhunkumarS@users.noreply.github.com>
2018-01-20 23:09:22 +0530

    Create sum_avg.c

---
+++ Assignment/sum_avg.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+	int a[10],i,n,sum=0,avg;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+a[i];
+	}
+	avg=sum/2;
+	printf("sum=%d",sum);
+	printf("avg=%d",avg);
+	return 0;
+}
